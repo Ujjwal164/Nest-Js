2ee1603527d38f1bd4714688e400b8f79b9d7031
Ujjwal164 <ujjwalsaini164@gmail.com>
2025-04-07 11:33:33 +0530

    exception handler

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -9,6 +9,15 @@ import { ApiOperation, ApiTags } from '@nestjs/swagger';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Get('all')
+  @ApiOperation({
+    summary: 'Get all users registered on the application',
+  })
+  getAllUsers() {
+    const result = this.userService.getAllUser();
+    return result;
+  }
+
   @Get(':id')
   @ApiOperation({
     summary: 'Get user registered on the application using their id',
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,7 +1,14 @@
-import { Inject, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+  RequestTimeoutException,
+} from '@nestjs/common';
 import { User } from './user.entity';
 import { UserBodyDto } from './dto/body.dto';
-import { Repository } from 'typeorm';
+import { DataSource, Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ConfigService, ConfigType } from '@nestjs/config';
 import profileConfig from './config/profile.config';
@@ -16,25 +23,94 @@ export class UserService {
 
     @Inject(profileConfig.KEY)
     private readonly profileConfigService: ConfigType<typeof profileConfig>, // this is used to inject the config from the config file
+
+    private readonly dataSource: DataSource, // we inject the datasource to use it in the service for transaction we use this
   ) {}
-  getAll(id: number) {
-    console.log(this.profileConfigService.profile);
-    return this.userRepository.find({
-      where: { id },
-      relations: { post: true },
-    });
+
+  async getAllUser() {
+    let users: User[] | null = null;
+    try {
+      users = await this.userRepository.find({
+        relations: { post: true },
+      });
+    } catch (error) {
+      throw new HttpException('Error while fetching users', HttpStatus.GONE);
+    }
+
+    if (users.length === 0) {
+      throw new BadRequestException('No users found');
+    } else {
+      return users;
+    }
   }
+
+  async getAll(id: number) {
+    let user: User | null = null;
+    try {
+      user = await this.userRepository.findOne({
+        where: { id },
+        relations: { post: true },
+      });
+    } catch (error) {
+      throw new RequestTimeoutException('Error while fetching user', error);
+    }
+    if (!user) {
+      throw new BadRequestException('User not found');
+    } else {
+      return user;
+    }
+  }
+
   async createUser(req: UserBodyDto) {
-    const check = await this.userRepository.findOne({
-      where: {
-        email: req.email,
-      },
-    });
+    let check: User | null = null;
+
+    try {
+      check = await this.userRepository.findOne({
+        where: {
+          email: req.email,
+        },
+      });
+    } catch (error) {
+      throw new RequestTimeoutException(
+        'Error while checking for existing user',
+        error,
+      );
+    }
+
     if (check) {
-      return 'User already exists';
+      throw new BadRequestException('user already exist in our database');
     } else {
       const newUser = this.userRepository.create(req);
       return this.userRepository.save(newUser);
+    }
+  }
+
+  async createManyUser(req: UserBodyDto[]) {
+    const newUsers: User[] = [];
+
+    // create query runner
+    const QueryRunner = this.dataSource.createQueryRunner();
+
+    //connect queryrunner to the database
+    await QueryRunner.connect();
+
+    // start transaction
+    await QueryRunner.startTransaction();
+
+    try {
+      for (const user of req) {
+        const createUser = QueryRunner.manager.create(User, user);
+        const result = await QueryRunner.manager.save(createUser);
+        newUsers.push(result);
+        await QueryRunner.commitTransaction();
+      }
+    } catch (error) {
+      // rollback transaction
+      await QueryRunner.rollbackTransaction();
+      throw new BadRequestException('Error while creating user', error);
+    } finally {
+      // release query runner
+      await QueryRunner.release();
     }
   }
 
